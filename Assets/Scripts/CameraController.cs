367f986917c03539d748b4150a9f679dd8a3c614
ngocanh2606 <150002936+ngocanh2606@users.noreply.github.com>
2024-06-04 11:53:30 +0700

    UI

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private Transform player;
+    [SerializeField] private Transform player, firstBackground, secondBackground, thirdBackground, fourthBackground;
+    private Vector2 lastPosition;
+
+    void Start()
+    {
+        lastPosition = transform.position;
+    }
 
     private void Update()
     {
         transform.position = new Vector3(player.position.x +2, player.position.y, transform.position.z);
+        Vector2 amountToMove = new Vector2(transform.position.x - lastPosition.x, transform.position.y - lastPosition.y);
+        firstBackground.position = firstBackground.position + new Vector3(amountToMove.x, amountToMove.y, 0);
+        secondBackground.position = secondBackground.position + new Vector3(amountToMove.x, amountToMove.y, 0) * 0.7f;
+        thirdBackground.position = thirdBackground.position + new Vector3(amountToMove.x, amountToMove.y, 0) * 0.5f;
+        fourthBackground.position = fourthBackground.position + new Vector3(amountToMove.x, amountToMove.y, 0) * 0.2f;
+        lastPosition = transform.position;
     }
 }
